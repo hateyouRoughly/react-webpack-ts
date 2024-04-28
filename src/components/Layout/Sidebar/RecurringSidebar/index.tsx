d5948cbc18c4bc98a89073bff967db80022b4909
Deepak Singh <deepak.singh@intimetec.com>
2024-04-28 22:51:14 +0530

    feat: app-layout arrow added for side manu drop-down

--- src/components/Layout/Sidebar/RecurringSidebar/index.tsx
+++ src/components/Layout/Sidebar/RecurringSidebar/index.tsx
@@ -79,7 +79,7 @@ const NavbarCollapse = (e: SidebarContent) => {
           <div className="col-auto p-0">{e.icon}</div>
           <div className="col p-0">&nbsp;{e.sidebarExtend && e.title}</div>
           {e.sidebarExtend && (
-            <div className="col-auto">
+            <div className="col-auto p-0">
               {open ? (
                 <ArrowDropUp htmlColor="white" />
               ) : (
