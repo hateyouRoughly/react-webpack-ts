8ea2a9b45dd5f0a0c244e03962ae730e3425a53e
Deepak Singh <deepak.singh@intimetec.com>
2024-04-29 14:34:45 +0530

    feat: chunck-optimization added chunck optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
-delete process.env.TS_NODE_PROJECT;
-
 const path = require("path");
 const webpack = require("webpack");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env) => {
   const mode = env?.mode == "production" ? "production" : "development";
@@ -13,13 +12,18 @@ module.exports = (env) => {
     mode, // Adjust to 'production' for production builds
     entry: "./src/index.tsx", // Main entry point
     output: {
-      filename: 'bundle.js',
+      filename: '[name].js',
       path: path.resolve(__dirname, "dist"), // Output directory
     },
     performance: {
       hints: false,
       maxEntrypointSize: 512000,
       maxAssetSize: 512000
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
     },
     module: {
       rules: [
@@ -50,7 +54,19 @@ module.exports = (env) => {
           ),
         },
       }),
-      // Add plugins as needed (e.g., for hot reloading)
+      new CopyPlugin({
+        patterns: [
+          { 
+            from: './public', 
+            to: './',
+            filter: (resourcePath) => !resourcePath.endsWith('/index.html') // Exclude based on path
+          } // Copy all public folder assets to dist/public
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html', // Path to your HTML template
+        chunks: ['main', '[name]'],  // Include main chunk (bundle.js) and named chunks
+      })
     ],
     devServer: {
       static: {
